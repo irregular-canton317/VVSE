05893e30c001d44247acff9441dfa6ee5b53ddbf
Niklas <dieeinzigenkisten@gmail.com>
2026-04-24 21:39:19 +0200

    Update v3

--- main.sh
+++ main.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-f=$1;t=$(stty -g);stty raw -echo;trap 'stty $t' 0;E=$(printf '\033');p=printf
+f=$1;t=$(stty -g);stty raw -echo;trap 'stty $t' 0;E=$(printf '\033');p=printf;v=1
 l=$(cat "$f" 2>/dev/null);r="";N="
-";W="Welcome! Help: fckaf.de/gZO$N"
+"
 while :;do
-z="$W$l|$r";[ "$s" ]&&z=$(echo "$z"|sed "s/$s/${E}[43;30m&${E}[0m/g")
-$p "${E}[H${E}[J$(echo "$z"|sed 's/$/\r/')${E}[99H${E}[7m VVSE|^S Save|^O Open|^X Exit|^U Clr|^F Find ${E}[m"
-k=$(dd bs=1 count=1 2>/dev/null);W=""
+z="$l|$r";[ "$s" ]&&z=$(echo "$z"|sed "s/$s/${E}[43;30m&${E}[0m/g")
+$p "${E}[H${E}[J$(echo "$z"|sed 's/$/\r/')"
+[ $v -eq 1 ]&&$p "${E}[99H${E}[7mVVSE|^S Sve|^O Opn|^X Ext|^U Clr|^F Fnd|^H HUI ${E}[m"
+k=$(dd bs=1 count=1 2>/dev/null)
 case "$k" in
 $($p '\030'))exit;;
+$($p '\010'))v=$((1-v));;
 $($p '\017'))stty $t;$p "${E}[99H${E}[2KOpen: ";read f;l=$(cat "$f" 2>/dev/null);r="";stty raw -echo;;
 $($p '\025'))l="";r="";;
 $($p '\006'))stty $t;$p "${E}[99H${E}[2KFind: ";read s;stty raw -echo;;
